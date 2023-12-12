1ee196b07e1f23fd8a5547bdf86e33bc23d27723
Henpai Hsu <dreamfliper@gmail.com>
2023-12-12 22:07:49 +0800

    feat: before nativewind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,45 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native'
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import EditScreenInfo from '../../components/EditScreenInfo'
+import { Text, View } from '../../components/Themed'
+import createClient from 'openapi-fetch'
+import { paths } from '../../src/lib/api/v1'
+
+const { GET, PUT } = createClient<paths>({ baseUrl: 'https://api.devptt.dev' })
+
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGkiOiJ7XCJjXCI6XCJ0ZXN0X2NsaWVudF9pZFwiLFwidFwiOlwiYXBwXCJ9IiwiZXhwIjoxNzAyNDcwNjY3LCJzdWIiOiJkcmVhbWZsaXBlciJ9.sgvkYgq6KQWeGA5v5Mm9aNFiq2KZ0gDovUEfM4gm_M0'
+
+const getFavorites = () =>
+  GET('/api/user/{user_id}/favorites', {
+    params: { path: { user_id: 'dreamfliper' } },
+    headers: { Authorization: `Bearer ${token}` }
+  })
 
 export default function TabOneScreen() {
+  getFavorites().then(console.log)
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: 'bold'
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
-  },
-});
+    width: '80%'
+  }
+})
---
+++ constants/Config.ts
@@ -0,0 +1,4 @@
+export default {
+  client_secret: 'test_client_secret',
+  client_id: 'test_client_id'
+} as const
